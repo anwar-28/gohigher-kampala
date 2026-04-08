c2ee0c0a5e4339ebea19685cc745acab0271b0f6
Kiggundu Shaafie Anwar <153352789+anwar-28@users.noreply.github.com>
2026-04-08 22:45:16 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['cloud.appwrite.io'],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // ignores ESLint errors during build
+  },
+  typescript: {
+    ignoreBuildErrors: true, // ignores TypeScript errors during build
+  },
 }
 
 module.exports = nextConfig
